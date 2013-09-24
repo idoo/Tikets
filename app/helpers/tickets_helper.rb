3010d498998648766177b2161b13c86c859344c8
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-09-24 15:25:53 +0400

    tree2hash

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,27 +1,21 @@
 module TicketsHelper
-  def get_tree_children tree, opts={}
-    node  = opts[:node]
-    boost = opts[:boost]
-    children = boost[node.id]
-  end
-
   def build_hash_tree(tree, options={}, result=[])
-    result = Array.wrap result
-    tree   = Array.wrap tree
+    result = []
 
     opts = {
-      node:      nil, # node
+      node:      nil,        # node
       parent_id: :parent_id, # parent id
-      boost:     [] # BOOST! array
+      boost:     []          # BOOST! array
     }.merge!(options)
 
-    node = opts[:node]
+    node      = opts[:node]
+    parent_id = opts[:parent_id].to_sym
 
     if opts[:boost].empty?
-      tree.each do |i|
-        num = i.ticketable_id || 0
+      tree.each do |node|
+        num = node[parent_id] || 0
         opts[:boost][num] = [] unless opts[:boost][num]
-        opts[:boost][num].push i
+        opts[:boost][num].push node
       end
     end
 
@@ -30,8 +24,8 @@ module TicketsHelper
 
       # define roots, if it's need
       if roots.nil? && !tree.empty?
-        min_parent_id = tree.map(opts[:parent].to_sym).compact.min
-        roots = tree.select{ |elem| elem[opts[:parent_id]] == min_parent_id }
+        min_parent_id = tree.map(parent_id).compact.min
+        roots = tree.select{ |elem| elem[parent_id] == min_parent_id }
       end
 
       # children rendering
@@ -43,19 +37,8 @@ module TicketsHelper
       end
     else
       # children rendering
-      children_res = []
-      children = opts[:boost][node.id]
-
-      unless children.nil?
-        children.each do |elem|
-          # _opts = opts.merge({node: elem, boost: opts[:boost]})
-          children_res.push children #get_tree_children(children)
-        end
-      end
-
-      result.push children_res
-      # _opts = opts.merge({nodes: node, :children => children_res})
-      # result.push build_hash_tree(self, _opts, result)
+      children = opts[:boost][node.id] || []
+      result.push({node: node, children: children })
     end
 
     result
